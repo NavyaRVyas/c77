ac61e4b0c394a5081b688d83d3064fa241b47e72
NavyaRVyas <122061293+NavyaRVyas@users.noreply.github.com>
2023-09-12 19:01:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var first_name="Navya"
+var last_name="Vyas"
+
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+
+var a=5;
+var b=10;
+
+var add= a+b;
+console.log(add);
+
+var sub= a-b;
+console.log(sub);
+
+var div= a/b;
+console.log(div);
+
+var multiplication= a*b;
+console.log(multiplication);
